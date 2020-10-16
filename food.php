2fe4737228aef8fa38839ccc37c61e2bafa40959
ridaadila <ridaadila10@gmail.com>
2020-10-17 01:40:01 +0700

    finishing local food page

---
+++ food.php
@@ -0,0 +1,149 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel">
+                <div class="panel-heading" style="text-align: center; background-color: blanchedalmond;">
+                    <span class="panel-icon"><i class="glyphicon glyphicon-cutlery"></i></span>
+                    <span class="panel-title"> Local Food in Surabaya</span>
+                </div>
+                <div class="panel-body" style="background-color: cadetblue;">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/soto.jpg" alt="soto">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Soto</h3>
+                                                <p>Recommended Place : Soto Cak Har, Soto Pak Sadi</p>  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/rawon.jpg" alt="rawon">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Rawon</h3>
+                                                <p>Recommended Place : Rawon Kalkulator Taman Bungkul, Rawon Pak Pangat</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <br> <br>
+
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/rujak.jpg" alt="soto">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Rujak Cingur</h3>
+                                                <p>Recommended Place : Rujak Cingur Achmad Jais, Rujak Cingur Genteng</p>  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/sateklopo.jpg" alt="rawon">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Sate Klopo</h3>
+                                                <p>Recommended place : Sate Klopo Ondomohen Bu Asih
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <br> <br>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/lontongbalap.jpg" alt="soto">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Lontong Balap</h3>
+                                                <p>Recommended Place : Depot Cak Erwin-Indrapura</p>  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/tahucampur.jpg" alt="rawon">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Tahu Campur</h3>
+                                                <p>Recommended place : Tahu Campur Kalasan
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <br> <br>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/bebek.jpg" alt="soto">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Nasi Bebek</h3>
+                                                <p>Recommended Place : Bebek Sinjay, Bebek Pak Janggut</p>  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/krawu.jpg" alt="rawon">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Nasi Krawu</h3>
+                                                <p>Recommended place : Nasi Krawu Bu Tiban
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <br> <br>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/tahutek.jpg" alt="soto">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Tahu Tek</h3>
+                                                <p>Recommended Place : Tahu Tek Telur Pak Jayen</p>  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-offset-1 col-md-4">
+                                        <div class="card-content">
+                                            <div class="card-img">
+                                                <img src="foto/sopkaki.jpg" alt="rawon">
+                                            </div>
+                                            <div class="card-desc">
+                                                <h3>Sop Kaki Kambing Sapi</h3>
+                                                <p>Recommended place : Sop Kaki Kambing Sapi Bang Oya
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <br> <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

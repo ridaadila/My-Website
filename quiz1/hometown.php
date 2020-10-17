dd4556fc1916f6a0aa51c619f5634eb423ec2b1d
ridaadila <ridaadila10@gmail.com>
2020-10-17 14:38:54 +0700

    fixxx

--- quiz1/hometown.php
+++ quiz1/hometown.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>Hometown</title>
     
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../bootstrap/fonts/font-awesome.min.css">
